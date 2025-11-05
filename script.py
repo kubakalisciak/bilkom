a28163138eb929aea091f629a437cd3e4177b117
kubakalisciak <168641088+kubakalisciak@users.noreply.github.com>
2025-11-05 20:04:37 +0100

    Build the scraper to extract data from bilkom

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Step 1: Fetch the page
-url = "https://quotes.toscrape.com/"
+station_id = "5100579"
+url = f"https://bilkom.pl/stacje/tablica?stacja={station_id}&przyjazd=false&_csrf="
 response = requests.get(url)
 
 # Check that it worked
@@ -13,11 +13,9 @@ else:
 
 soup = BeautifulSoup(html, "lxml")
 
-# Step 2: Find all quote containers
-quotes = soup.find_all("div", class_="quote")
+next_train = soup.find_all("li", class_="el")[:2]
 
-# Step 3: Loop through and print results
-for quote in quotes:
-    text = quote.find("span", class_="text").get_text()
-    author = quote.find("small", class_="author").get_text()
-    print(f"{text} — {author}")
+print(next_train)
+
+# todo: extract each trains data into the dict
+# ! use .select_one()
